310d5aac0635bfc6fc9784db84edf6011fd64fca
TeeQiJing <qijingtee1227@gmail.com>
2024-10-23 18:17:58 +0800

    First Commit

--- app/src/main/java/com/practical/profilepageshopee/MainActivity.java
+++ app/src/main/java/com/practical/profilepageshopee/MainActivity.java
@@ -2,7 +2,9 @@ package com.practical.profilepageshopee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView oriPrice1 = findViewById(R.id.oriPrice1);
+        oriPrice1.setPaintFlags(oriPrice1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        TextView oriPrice2 = findViewById(R.id.oriPrice2);
+        oriPrice2.setPaintFlags(oriPrice2.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        TextView oriPrice3 = findViewById(R.id.oriPrice3);
+        oriPrice3.setPaintFlags(oriPrice3.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
     }
 }
